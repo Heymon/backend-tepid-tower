96596f0c8ed5e34719f3a65623a1d963c1ab7c05
Heymon <rcavalleiro@gmail.com>
2021-02-14 15:01:52 -0800

    refactors leaderboard controller so that sorting happens in db

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -12,10 +12,14 @@ router.get("/global", async function (req, res) {
     try {   
 
             console.log("looking hereer");
-            // console.log(req);
-            const curUsers = await db.User.find({}).populate("profile");
+            const curProfiles = await db.Profile.find({}).sort({ highscore : -1 }).limit(12);
+            // const curProfiles = await db.Profile.find({$query: {}, $orderby: { highscore : 1 }});
+            curProfiles.map((item) => {
 
-            return res.status(200).json({status: 200, curUsers});
+                console.log(item.highscore);
+            })
+
+            return res.status(200).json({status: 200, curProfiles});
        
     } catch (error) {
         res.status(500).json({status: 500, message:"Something went wrong", error});
